2505e37ca6bd3a5b19c0c92dae91601cdd81d191
AustinLichtenstein <71053819+AustinLichtenstein@users.noreply.github.com>
2020-09-15 01:46:00 -0500

    Add files via upload
    HomeWork#3 Austin Lichtenstein

---
+++ DecimaltoBinary.java
@@ -0,0 +1,37 @@
+package hw3;
+
+import java.util.Scanner;
+
+public class DecimaltoBinary {
+
+    public static void main(String[] args) {
+        int number;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter decimal number ");
+        number = in.nextInt();
+
+        if (number < 0) {
+            System.out.println("Error: Not a decimal");
+        } else {
+
+            System.out.print("binary is:");
+            //System.out.print(binaryform(number));
+            printBinaryform(number);
+        }
+    }
+
+    private static void printBinaryform(int number) {
+        int remainder;
+
+        if (number <= 1) {
+            System.out.print(number);
+            return; // KICK OUT OF THE RECURSION
+        }
+
+        remainder = number % 2;
+        printBinaryform(number >> 1);
+        System.out.print(remainder);
+    }
+}
